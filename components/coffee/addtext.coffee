0eff5cacd572fba79471e1c763c393e7dd01e862
razvantbf <razvan@thebasicfundamentals.com>
2014-12-27 10:48:49 +0200

    Angular display data from JSON

--- components/coffee/addtext.coffee
+++
@@ -1,4 +0,0 @@
-$ = require('jquery')
-
-$ ->
-	$('.helloworld').css("color", "red");
--- components/scripts/addtext.js
+++
@@ -1,7 +0,0 @@
-var $;
-
-$ = require('jquery');
-
-$(function() {
-  return $('.helloworld').css("color", "red");
-});
